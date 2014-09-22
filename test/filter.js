591058240a8edbb03e4a2dea8bf11f3a10e794e1
ajlopez <webmaster@ajlopez.com>
2014-09-22 08:01:06 -0300

    filter object properties

--- test/filter.js
+++ test/filter.js
@@ -22,3 +22,13 @@ exports['filter odd values from object'] = function (test) {
     test.deepEqual(result, { a: 1, c: 3, e: 5 });
 }
 
+exports['filter properties from object'] = function (test) {
+    var result = sc.filter(
+        function (x, name) { return name == 'a' || name == 'c'; }, 
+        { a: 1, b: 2, c: 3, d: 4, e: 5 });
+    
+    test.ok(result);
+    test.ok(!Array.isArray(result));
+    test.equal(typeof result, 'object');
+    test.deepEqual(result, { a: 1, c: 3 });
+}
