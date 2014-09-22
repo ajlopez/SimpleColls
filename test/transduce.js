c841acd463c7d05cb21bd8d21f194a400eb52ceb
ajlopez <webmaster@ajlopez.com>
2014-09-22 07:51:12 -0300

    transduce additional tests

--- test/transduce.js
+++ test/transduce.js
@@ -15,3 +15,32 @@ exports['transduce an array'] = function (test) {
     test.ok(Array.isArray(result));
     test.deepEqual(result, [2, 3, 4]);
 }
+
+exports['transduce an array to sum values'] = function (test) {
+    var result = sc.transduce(
+        function (x) { return x + 1}, 
+        function (result, value) {
+            result += value;
+            return result;
+        },
+        0,
+        [1, 2, 3]);
+        
+    test.ok(result);
+    test.equal(result, 9);
+}
+
+exports['transduce an object to sum values'] = function (test) {
+    var result = sc.transduce(
+        function (x) { return x + 1}, 
+        function (result, value) {
+            result += value;
+            return result;
+        },
+        0,
+        { a: 1, b: 2, c: 3 });
+        
+    test.ok(result);
+    test.equal(result, 9);
+}
+
