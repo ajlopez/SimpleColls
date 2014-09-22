177ad7874b7e54c31e20b9cc20c7820b4365e462
ajlopez <webmaster@ajlopez.com>
2014-09-22 07:59:37 -0300

    filter test on object

--- test/filter.js
+++ test/filter.js
@@ -10,3 +10,15 @@ exports['filter odd numbers from array'] = function (test) {
     test.ok(Array.isArray(result));
     test.deepEqual(result, [1, 3, 5]);
 }
+
+exports['filter odd values from object'] = function (test) {
+    var result = sc.filter(
+        function (x) { return x % 2 == 1; }, 
+        { a: 1, b: 2, c: 3, d: 4, e: 5 });
+    
+    test.ok(result);
+    test.ok(!Array.isArray(result));
+    test.equal(typeof result, 'object');
+    test.deepEqual(result, { a: 1, c: 3, e: 5 });
+}
+
