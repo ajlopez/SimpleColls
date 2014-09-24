3601ca731c06421a0c5bcdba6c3bbb1db26d91ad
ajlopez <webmaster@ajlopez.com>
2014-09-24 10:00:44 -0300

    reduce additional tests

--- test/reduce.js
+++ test/reduce.js
@@ -5,11 +5,34 @@ exports['sum an array'] = function (test) {
     var result = sc.reduce(
         function (result, value) {
             return result + value;
-            result.push(value);
-            return result;
         },
         0,
         [1, 2, 3]);
+        
+    test.ok(result);
+    test.equal(result, 6);
+}
+
+exports['transform an array'] = function (test) {
+    var result = sc.reduce(
+        function (result, value) {
+            result.push(value + 1);
+            return result;
+        },
+        [],
+        [1, 2, 3]);
+        
+    test.ok(result);
+    test.deepEqual(result, [2, 3, 4]);
+}
+
+exports['sum an object'] = function (test) {
+    var result = sc.reduce(
+        function (result, value) {
+            return result + value;
+        },
+        0,
+        { a: 1, b: 2, c: 3 });
         
     test.ok(result);
     test.equal(result, 6);
