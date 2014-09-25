c93b532ad056bfab9bf0636e38460f6e8e67df94
ajlopez <webmaster@ajlopez.com>
2014-09-25 07:35:52 -0300

    map object with next

--- test/map.js
+++ test/map.js
@@ -18,4 +18,21 @@ exports['map object'] = function (test) {
     test.deepEqual(result, { a: 2, b: 3 });
 }
 
-
+exports['map object with next'] = function (test) {
+    var counter = 0;
+    
+    var obj = {
+        next: function () { if (counter >= 3) return null; return counter++; }
+    }
+    
+    var result = sc.map(function (x) { return x + 1 }, obj);
+    
+    test.ok(result);
+    test.ok(result.next);
+    test.equal(typeof result.next, 'function');
+    
+    test.equal(result.next(), 1);
+    test.equal(result.next(), 2);
+    test.equal(result.next(), 3);
+    test.equal(result.next(), null);
+}
