6373e10881d0285ce8581d5a034bd8b6248b3f64
ajlopez <webmaster@ajlopez.com>
2014-09-25 07:42:14 -0300

    reduce processing an object with next

--- test/reduce.js
+++ test/reduce.js
@@ -13,6 +13,24 @@ exports['sum an array'] = function (test) {
     test.equal(result, 6);
 }
 
+exports['sum an object with next'] = function (test) {
+    var counter = 0;
+    
+    var obj = {
+        next: function () { if (counter >= 3) return null; return ++counter; }
+    }
+    
+    var result = sc.reduce(
+        function (result, value) {
+            return result + value;
+        },
+        0,
+        obj);
+        
+    test.ok(result);
+    test.equal(result, 6);
+}
+
 exports['transform an array'] = function (test) {
     var result = sc.reduce(
         function (result, value) {
