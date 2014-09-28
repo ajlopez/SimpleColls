fd643a00629c96be60c5c3c5b2ad7604de49ce36
ajlopez <webmaster@ajlopez.com>
2014-09-28 07:29:54 -0300

    range filter primes test

--- test/range.js
+++ test/range.js
@@ -11,3 +11,35 @@ exports['range one to three'] = function (test) {
     test.equal(result.next(), 3);
     test.equal(result.next(), null);
 }
+
+exports['range naturals from one'] = function (test) {
+    var result = sc.range(1);
+    
+    test.ok(result);
+
+    for (var k = 1; k < 1000; k++)
+        test.equal(result.next(), k);
+}
+
+exports['range primes'] = function (test) {
+    function isprime(n) {
+        for (var k = 2; k * k <= n; k++)
+            if (n % k == 0)
+                return false;
+         
+        return true;
+    }
+    
+    var result = sc.range(1).filter(isprime);
+    
+    test.ok(result);
+
+    test.equal(result.next(), 1);
+    test.equal(result.next(), 2);
+    test.equal(result.next(), 3);
+    test.equal(result.next(), 5);
+    test.equal(result.next(), 7);
+    test.equal(result.next(), 11);
+    test.equal(result.next(), 13);
+    test.equal(result.next(), 17);
+}
